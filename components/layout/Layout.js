e2890f3ead584a53fb90d43e4d3ac6a2597aa34c
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-04 11:53:31 +0330

    layout component done

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,7 +1,23 @@
-import styles from "./Layout.css"
-const Layout = () => {
+import Link from "next/link"
+import styles from "./Layout.module.css"
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <>
+    <header className={styles.header}>
+        <div className={styles.left}>
+            <Link href='/'>MyFood</Link>
+        </div>
+        <div className={styles.right}>
+            <Link href="/menu">Menu</Link>
+            <Link href="/categories">Categories</Link>
+        </div>
+    </header>
+    <div className={styles.container}>{children}</div>
+    <footer className={styles.footer}>
+        <a href="https://github.com/ParsaDokhtMohammadi/" target="_blank" rel="norefferer">Parsa dokht mohammadi  </a>
+        nextJs | foodRecipe project &copy;
+    </footer>
+    </>
   )
 }
 
