9f0a87ec7b88f583ab09aa762c62d7409e7573d6
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-07-05 15:38:49 +0330

    categories page server side filtering added

---
+++ pages/categories/index.js
@@ -0,0 +1,76 @@
+import styles from "@/styles/CategoriesPage.module.css"
+import { useRouter } from "next/router"
+import { useState } from "react"
+
+const Categories = ({ data }) => {
+    console.log(data)
+    const router = useRouter()
+    const [query, setQuery] = useState({ difficulty: "", time: "" })
+
+    const changeHandler = (e) => {
+        setQuery({ ...query, [e.target.name]: e.target.value })
+    }
+
+    const searchHandler = () => {
+        router.push({ pathname: "/categories", query }) 
+    }
+
+    return (
+        <div className={styles.container}>
+            <h2>Categories</h2>
+            <div className={styles.subContainer}>
+                <div className={styles.select}>
+                    <select value={query.difficulty} name="difficulty" onChange={changeHandler}>
+                        <option value={""}>Difficulty</option>
+                        <option value={"Easy"}>Easy</option>
+                        <option value={"Medium"}>Medium</option>
+                        <option value={"Hard"}>Hard</option>
+                    </select>
+                    <select value={query.time} name="time" onChange={changeHandler}>
+                        <option value={""}>Cooking Time</option>
+                        <option value={"More"}>More Than 30 Min</option>
+                        <option value={"Less"}>Less Than 30 Min</option>
+                    </select>
+                    <button onClick={searchHandler}>Search</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Categories
+
+export async function getServerSideProps(context) {
+    const { query: { difficulty, time } } = context
+    const res = await fetch("http://localhost:4000/data")
+    const data = await res.json()
+
+    const filteredData = data.filter(item => {
+        const DifficultyResult = difficulty
+            ? item.details.filter(detail =>
+                detail.Difficulty && detail.Difficulty === difficulty
+            )
+            : item.details
+
+        const TimeResult = time
+            ? item.details.filter(detail => {
+                const cookingTime = detail["Cooking Time"] || ""
+                const [TimeDetail] = cookingTime.split(" ")
+                const num = parseInt(TimeDetail)
+                if (isNaN(num)) return false
+                if (time.toLowerCase() === "less" && num <= 30) {
+                    return true
+                } else if (time.toLowerCase() === "more" && num > 30) {
+                    return true
+                }
+                return false
+            })
+            : item.details
+
+        return DifficultyResult.length && TimeResult.length
+    })
+
+    return {
+        props: { data: filteredData }
+    }
+}
